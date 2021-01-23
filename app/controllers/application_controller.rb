f6d25e9eee4556d72882d1d9b5227fb829fd0ddb
Hiroyuk Ishizuka <hiroyuki.1207.18@gmail.com>
2021-01-23 19:32:11 +0900

    [accept]username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_permitted_paramaters, if: :devise_controller?
+
+	protecgted
+	def configure_permitted_paramaters
+		devise_paramater_sanitizer.permit(:sign_up keys: [:username])
+	end
 end
