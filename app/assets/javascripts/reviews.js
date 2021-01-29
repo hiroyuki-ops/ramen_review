48c0a84179cc1eb349e857db026b7fd416ae8e0d
Hiroyuk Ishizuka <hiroyuki.1207.18@gmail.com>
2021-01-29 14:27:20 +0900

    [add]mouseout

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,6 +1,6 @@
 const stars = document.querySelector(".ratings").children;
 const ratingValue = document.getElementById(".rating-balue");
-const ratinBalueDisplay = docment.getElementById(".rating-value-display");
+const ratinValueDisplay = docment.getElementById(".rating-value-display");
 
 let index;
 
@@ -15,4 +15,20 @@ for(let i=0; i<stars.length; i++){
 			stars[j].classList.add("fa-star");
 		}
 	})
+	stars[i].addEventListener("click", function(){
+		ratingValue.value = i + 1;
+		ratinValueDisplay.textContent = retingValue.value;
+		index = i;
+	})
+	stars[i].addEventLiestener("mouseout", function(){
+		for(let j=0; j<stars.length; j++){
+			stars[j].classList.remobe("fa-star");
+			stars[j].classList.add("fa-star-o");
+		}
+		for(let j=0; j<=index; j++){
+			stars[j].classList.remobe("fa-star-o");
+			stars[j].classList.add("fa-star");
+		}
+	})
+
 }
